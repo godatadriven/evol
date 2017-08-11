b7028a767fdbe83c1bc19c02b14bfb540bc4979c
koaning <vincentwarmerdam@gmail.com>
2017-08-11 20:12:39 +0200

    proper setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='evol',
+      version='0.1',
+      description='A Grammar for Evolutionary Algorithms and other Heuristics',
+      author='Vincent D. Warmerdam',
+      author_email='vincentwarmerdam@gmail.com',
+      url='https://github.com/koaning/evol',
+      packages=['evol',],
+      install_requires=['pytest'],)
