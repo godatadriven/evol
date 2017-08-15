033d4358bfcc1a0af57e1c3561d494c33dc9f568
Rogier van der Geer <rogiervandergeer@godatadriven.com>
2017-08-15 17:08:30 +0200

    Add docstring

--- evol/helpers/utils.py
+++ evol/helpers/utils.py
@@ -2,6 +2,16 @@ from inspect import signature
 
 
 def select_arguments(func):
+    """Decorate a function such that it accepts any keyworded arguments.
+
+    The resulting function accepts any arguments, but only arguments that
+    the original function accepts are passed. This allows keyworded 
+    arguments to be passed to multiple (decorated) functions, even if they
+    do not (all) accept these arguments.
+
+    :param func: Function to decorate.
+    :return: Callable
+    """
     def result(*args, **kwargs):
         try:
             return func(*args, **kwargs)
