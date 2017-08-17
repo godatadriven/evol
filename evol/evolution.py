495d4f71dc3d296ddee81c4427615ee910382ed2
koaning <vincentwarmerdam@gmail.com>
2017-08-17 08:41:48 +0200

    example of evolution buildup

--- evol/evolution.py
+++ evol/evolution.py
@@ -1,4 +1,6 @@
-
+from copy import copy
+from .population import Population
+from .step import EvaluationStep
 
 class Evolution:
 
@@ -7,3 +9,18 @@ class Evolution:
 
     def __iter__(self):
         return self.steps.__iter__()
+
+    def evaluate(self, name=None) -> 'Evolution':
+        if not name:
+            name = f"step-{len(self.steps)+1}-evaluate"
+        result = copy(self)
+        result.steps.append(EvaluationStep(name=name))
+        return result
+
+    def evolve(self, population: Population):
+        for step in self.chain:
+            population = step.apply(population, *step.args, **step.kwargs)
+
+    def __repr__(self):
+        result = "<Evolution object with steps>"
+        return result + "\n".join([f"  -{step.name} for step in self.steps"])
