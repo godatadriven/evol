707ead7a6d716f532238a8dd653a94303ca0fd04
Rogier van der Geer <rogiervandergeer@godatadriven.com>
2017-10-06 15:32:03 +0200

    Add combiner helpers

---
+++ evol/helpers/combiners.py
@@ -0,0 +1,8 @@
+
+
+def select_min_fitness(*parents):
+    return min(parents, key=lambda parent: parent.fitness)
+
+
+def select_max_fitness(*parents):
+    return max(parents, key=lambda parent: parent.fitness)
