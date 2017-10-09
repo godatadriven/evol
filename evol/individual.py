45a7e3c32fe5b29f5fe5b2c146738bdbfde85e77
Rogier van der Geer <rogiervandergeer@godatadriven.com>
2017-10-09 11:59:49 +0200

    Add docstrings to methods

--- evol/individual.py
+++ evol/individual.py
@@ -2,6 +2,7 @@ from uuid import uuid4
 
 
 class Individual:
+    """Represents an individual in a population. The individual has a chromosome."""
 
     def __init__(self, chromosome):
         self.age = 0
@@ -13,9 +14,22 @@ class Individual:
         return f"<individual id:{self.id} fitness:{self.fitness}>"
 
     def evaluate(self, eval_function, lazy=False):
+        """Evaluate the fitness of the individual.
+
+        :param eval_function: Function that reduces a chromosome to a fitness.
+        :type eval_function: Callable[float]
+        :param lazy: If True, do no re-evaluate the fitness if the fitness is known.
+        :type lazy: bool
+        """
         if self.fitness is None or not lazy:
             self.fitness = eval_function(self.chromosome)
 
     def mutate(self, func, **kwargs):
+        """Mutate the chromosome of the individual.
+
+        :param func: Function that accepts a chromosome and returns a mutated chromosome.
+        :type func: Callable[chromosome, **kwargs] -> chromosome
+        :param kwargs: Arguments to pass to the mutation function.
+        """
         self.chromosome = func(self.chromosome, **kwargs)
         self.fitness = None
