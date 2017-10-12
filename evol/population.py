47c5ed7a05926c3fc51f861254de4ad9b5bbb294
Rogier van der Geer <rogiervandergeer@godatadriven.com>
2017-10-12 12:50:04 +0200

    Add docstrings and rename args of ContestPopulation

--- evol/population.py
+++ evol/population.py
@@ -7,7 +7,16 @@ from evol.helpers.utils import select_arguments
 
 
 class Population:
+    """Population of Individuals
 
+    :param chromosomes: Collection of initial chromosomes of the Population.
+    :type chromosomes: Collection[chromosome]
+    :param eval_function: Function that reduces a chromosome to a fitness.
+    :type eval_function: Callable[chromosome] -> float
+    :param maximize: If True, fitness will be maximized, otherwise minimized.
+        Defaults to True.
+    :type maximize: bool
+    """
     def __init__(self, chromosomes, eval_function, maximize=True):
         self.eval_function = eval_function
         self.generation = 0
@@ -44,7 +53,13 @@ class Population:
         return cls(chromosomes=chromosomes, eval_function=eval_func)
 
     def evolve(self, evolution: 'Evolution', n: int = 1) -> 'Population':
-        """Evolve the population."""
+        """Evolve the population according to an Evolution.
+
+        :param evolution: Evolution to follow
+        :type evolution: Evolution
+        :param n:
+        :return:
+        """
         result = deepcopy(self)
         for evo_batch in range(n):
             for step in evolution:
@@ -52,37 +67,73 @@ class Population:
         return result
 
     def evaluate(self, lazy: bool=False) -> 'Population':
-        """Evaluate the individuals in the population
-        
-        :param lazy: If True, do not evaluate already evaluated individuals. Defaults to False.
+        """Evaluate the individuals in the population.
+
+        This evaluates the fitness of all individuals. If lazy is True, the
+        fitness is only evaluated when a fitness value is not yet known. In
+        most situations adding an explicit evaluation step is not needed, as
+        lazy evaluation is implicitly included in the operations that need it
+        (most notably in the survive operation).
+
+        :param lazy: If True, do no re-evaluate the fitness if the fitness is known.
         :type lazy: bool
-        :return: Population
+        :return: self
         """
         for individual in self.individuals:
             individual.evaluate(eval_function=self.eval_function, lazy=lazy)
         return self
 
     def apply(self, func, **kwargs) -> 'Population':
-        """apply(f(Population, **kwargs) -> Population, **kwargs)"""
+        """Apply the provided function to the population.
+
+        :param func: Function to apply to the population.
+        :type func: Callable[Population]
+        :param kwargs: Arguments to pass to the function.
+        :return: self
+        """
         return func(self, **kwargs)
 
     def map(self, func, **kwargs) -> 'Population':
-        """map(f(Individual, **kwargs) -> Individual, **kwargs)"""
+        """Apply the provided function to each individual in the population.
+
+        :param func: Function to apply to the individuals in the population.
+        :type func: Callable[Individual] -> Individual
+        :param kwargs: Arguments to pass to the function.
+        :return: self
+        """
         self.individuals = [func(individual, **kwargs) for individual in self.individuals]
         return self
 
     def filter(self, func, **kwargs) -> 'Population':
-        """filter(f(Individual, **kwargs) -> bool, **kwargs)"""
+        """Add a filter step to the Evolution.
+
+        Filters the individuals in the population using the provided function.
+
+        :param func: Function to filter the individuals in the population by.
+        :type func: Callable[Individual] -> bool
+        :param kwargs: Arguments to pass to the function.
+        :return: self
+        """
         self.individuals = [individual for individual in self.individuals if func(individual, **kwargs)]
         return self
 
-    def update(self, func, **kwargs) -> 'Population':
-        """update(f(**kwargs), **kwargs)"""
-        func(self, **kwargs)
-        return self
-
     def survive(self, fraction=None, n=None, luck=False) -> 'Population':
-        """survive(fraction=None, n=None, luck=False)"""
+        """Let part of the population survive.
+
+        Remove part of the population. If both fraction and n are specified,
+        the minimum resulting population size is taken.
+
+        :param fraction: Fraction of the original population that survives.
+            Defaults to None.
+        :type fraction: float/None
+        :param n: Number of individuals of the population that survive.
+            Defaults to None.
+        :type n: int/None
+        :param luck: If True, individuals randomly survive (with replacement!)
+            with chances proportional to their fitness. Defaults to False.
+        :type luck: bool
+        :return: self
+        """
         if fraction is None:
             if n is None:
                 raise ValueError('everyone survives! must provide either "fraction" and/or "n".')
@@ -105,9 +156,21 @@ class Population:
         return self
 
     def breed(self, parent_picker, combiner, population_size=None, **kwargs) -> 'Population':
-        """breed(parent_picker=f(Population) -> seq[individuals],
-                                             f(*seq[chromosome]) -> chromosome,
-                                                                    population_size = None, **kwargs)
+        """Create new individuals by combining existing individuals.
+
+        :param parent_picker: Function that selects parents.
+        :type parent_picker: Callable[list[Individual]] -> tuple[Individual]
+        :param combiner: Function that combines chromosomes into a new
+            chromosome. Must be able to handle the number of chromosomes
+            that the combiner returns.
+        :type combiner: Callable[chromosome, ...] -> chromosome
+        :param population_size: Intended population size after breeding.
+            If None, take the previous intended population size.
+            Defaults to None.
+        :type population_size: int/None
+        :param kwargs: Kwargs to pass to the parent_picker and combiner.
+            Arguments are only passed to the functions if they accept them.
+        :return: self
         """
         parent_picker = select_arguments(parent_picker)
         combiner = select_arguments(combiner)
@@ -124,26 +187,82 @@ class Population:
             # TODO: increase generation and individual's ages
         return self
 
-    def mutate(self, func, **kwargs) -> 'Population':
-        """mutate(f(chromosome) -> chromosome, ** kwargs)"""
+    def mutate(self, func, probability=1.0, **kwargs) -> 'Population':
+        """Mutate the chromosome of each individual.
+
+        :param func: Function that accepts a chromosome and returns
+            a mutated chromosome.
+        :type func: Callable[chromosome, **kwargs] -> chromosome
+        :param probability: Probability that the individual mutates.
+            The function is only applied in the given fraction of cases.
+            Defaults to 1.0.
+        :type probability: float
+        :param kwargs: Arguments to pass to the mutation function.
+        :return: self
+        """
         for individual in self.individuals:
-            individual.mutate(func, **kwargs)
+            individual.mutate(func, probability=probability, **kwargs)
         return self
 
 
 class ContestPopulation(Population):
-    def __init__(self, chromosomes, eval_function, matches_per_round=10, individuals_per_match=2, maximize=True):
+    """Population which is evaluated through contests.
+
+    This variant of the Population is used when individuals cannot be
+    evaluated on a one-by-one basis, but instead can only be compared to
+    each other. This is typically the case for AI that performs some task
+    (i.e. plays a game), but can be useful in many other cases.
+
+    For each round of evaluation, each individual participates in a given
+    number of contests, in which a given number of individuals take part.
+    The resulting scores of these contests are summed to form the fitness.
+
+    Since the fitness of an individual is dependent on the other individuals
+    in the population, the fitness of all individuals is recalculated when
+    new individuals are present, and the fitness of all individuals is reset
+    when the population is modified (e.g. by calling survive, mutate etc).
+
+    :param chromosomes: Collection of initial chromosomes of the Population.
+    :type chromosomes: Collection[chromosome]
+    :param eval_function: Function that reduces multiple chromosomes to a
+        set of scores.
+    :type eval_function: Callable[*chromosomes] -> tuple[float]
+    :param contests_per_round: Number of contests each individual takes part
+        in for each evaluation round. Defaults to 10.
+    :type contests_per_round: int
+    :param individuals_per_contest: Number of individuals that take part in
+        each contest. The size of the population must be divisible by this
+        number. Defaults to 2.
+    :type individuals_per_contest: int
+    :param maximize: If True, fitness will be maximized, otherwise minimized.
+        Defaults to True.
+    :type maximize: bool
+    """
+    def __init__(self, chromosomes, eval_function, contests_per_round=10, individuals_per_contest=2, maximize=True):
         Population.__init__(self, chromosomes=chromosomes, eval_function=eval_function, maximize=maximize)
-        self.matches_per_round = matches_per_round
-        self.individuals_per_match = individuals_per_match
+        self.contests_per_round = contests_per_round
+        self.individuals_per_contest = individuals_per_contest
 
     def evaluate(self, lazy: bool=False) -> 'ContestPopulation':
+        """Evaluate the individuals in the population.
+
+        This evaluates the fitness of all individuals. If lazy is True, the
+        fitness is only evaluated when a fitness value is not yet known for
+        all individuals.
+        In most situations adding an explicit evaluation step is not needed, as
+        lazy evaluation is implicitly included in the operations that need it
+        (most notably in the survive operation).
+
+        :param lazy: If True, do no re-evaluate the fitness if the fitness is known.
+        :type lazy: bool
+        :return: self
+        """
         if lazy and all(individual.fitness is not None for individual in self):
             return self
         for individual in self.individuals:
             individual.fitness = 0
-        for _ in range(self.matches_per_round):
-            offsets = [0] + [randint(0, len(self.individuals) - 1) for _ in range(self.individuals_per_match - 1)]
+        for _ in range(self.contests_per_round):
+            offsets = [0] + [randint(0, len(self.individuals) - 1) for _ in range(self.individuals_per_contest - 1)]
             generators = [islice(cycle(self.individuals), offset, None) for offset in offsets]
             for competitors in islice(zip(*generators), len(self.individuals)):
                 scores = self.eval_function(*competitors)
@@ -152,22 +271,57 @@ class ContestPopulation(Population):
         return self
 
     def map(self, func, **kwargs) -> 'Population':
-        """map(f(Individual, **kwargs) -> Individual, **kwargs)"""
+        """Apply the provided function to each individual in the population.
+
+        Resets the fitness of all individuals.
+
+        :param func: Function to apply to the individuals in the population.
+        :type func: Callable[Individual] -> Individual
+        :param kwargs: Arguments to pass to the function.
+        :return: self
+        """
         Population.map(self, func=func, **kwargs)
         self.reset_fitness()
         return self
 
     def filter(self, func, **kwargs) -> 'Population':
-        """filter(f(Individual, **kwargs) -> bool, **kwargs)"""
+        """Add a filter step to the Evolution.
+
+        Filters the individuals in the population using the provided function.
+        Resets the fitness of all individuals.
+
+        :param func: Function to filter the individuals in the population by.
+        :type func: Callable[Individual] -> bool
+        :param kwargs: Arguments to pass to the function.
+        :return: self
+        """
         Population.filter(self, func=func, **kwargs)
         self.reset_fitness()
         return self
 
     def survive(self, fraction=None, n=None, luck=False) -> 'ContestPopulation':
+        """Let part of the population survive.
+
+        Remove part of the population. If both fraction and n are specified,
+        the minimum resulting population size is taken. Resets the fitness
+        of all individuals.
+
+        :param fraction: Fraction of the original population that survives.
+            Defaults to None.
+        :type fraction: float/None
+        :param n: Number of individuals of the population that survive.
+            Defaults to None.
+        :type n: int/None
+        :param luck: If True, individuals randomly survive (with replacement!)
+            with chances proportional to their fitness. Defaults to False.
+        :type luck: bool
+        :return: self
+        """
         Population.survive(self, fraction=fraction, n=n, luck=luck)
         self.reset_fitness()
         return self  # If we return the result of Population.survive PyCharm complains that it is of type 'Population'
 
     def reset_fitness(self):
+        """Reset the fitness of all individuals."""
         for individual in self:
             individual.fitness = None
