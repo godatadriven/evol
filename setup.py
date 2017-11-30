4fa5089e0880433b340eaee23844826b3386eaeb
Warmerdam, Vincent <vincentwarmerdam@royalfloraholland.com>
2017-11-30 20:38:59 +0100

    author in setup.py does not allow list

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
-setup(name='evol',
-      version='0.1.1',
-      description='A Grammar for Evolutionary Algorithms and Heuristics',
-      author=['Vincent D. Warmerdam', 'Rogier van der Geer'],
-      author_email='vincentwarmerdam@gmail.com',
-      url='https://github.com/koaning/evol',
-      packages=['evol', 'evol.helpers'],
-      keywords=['genetic', 'algorithms', 'heuristics'],
-      install_requires=['pytest'],)
+setup(
+    name='evol',
+    version='0.1.1',
+    description='A Grammar for Evolutionary Algorithms and    Heuristics',
+    author='Vincent D. Warmerdam, Rogier van der Geer',
+    author_email='vincentwarmerdam@gmail.com',
+    url='https://github.com/koaning/evol',
+    packages=['evol', 'evol.helpers'],
+    keywords=['genetic', 'algorithms', 'heuristics'],
+    install_requires=['pytest'])
