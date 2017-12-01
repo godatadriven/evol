8baabd3a78db14e82b347d89140fbe76c5256b37
Rogier van der Geer <rogiervandergeer@godatadriven.com>
2017-12-01 14:44:14 +0100

    Improve select_partition

--- evol/helpers/combiners/_utils.py
+++ evol/helpers/combiners/_utils.py
@@ -4,14 +4,17 @@ from random import choice, randint
 from typing import Iterable, Tuple, Generator, Any, Set, List, Dict
 
 
-def select_partition(length: int) -> Tuple[int, int]:
+def select_partition(length: int, min_size: int=1, max_size: int=None) -> Tuple[int, int]:
     """Select a partition of a chromosome.
 
     :param length: Length of the chromosome.
+    :param min_size: Minimum length of the partition. Defaults to 1.
+    :param max_size: Maximum length of the partition. Defaults to length -1.
     :return: Start and end index of the partition.
     """
-    x, y = randint(0, length), randint(0, length)
-    return (x, y) if x < y else (y, x)
+    partition_size = randint(min_size, length-1 if max_size is None else max_size)
+    partition_start = randint(0, length - partition_size)
+    return partition_start, partition_start + partition_size
 
 
 def construct_neighbors(*chromosome: Tuple[Any]) -> defaultdict:
