3fd0af69e7ec811ecb2c0246f7551af7a035ead5
Warmerdam, Vincent <vincentwarmerdam@royalfloraholland.com>
2017-12-01 15:56:22 +0100

    v1 of docs

--- evol/__init__.py
+++ evol/__init__.py
@@ -4,7 +4,8 @@ Evol
 
 This documentation has been automatically generated via `pdoc`.
 
-Evol is a library that helps you make evolutionary algorithms. 
+Evol is a library that helps you make evolutionary algorithms. The
+goal is to have a library that is fun and clear to use, but not fast. 
 
 If you're looking at the library for the first time we recommend
 that you first take a look at the examples in the /examples folder
@@ -15,7 +16,33 @@ library makes it fun to write heuristics again.
  
 Contributing Guide
 ---------------------------------------
-TODO 
+
+### Local development
+
+Python can help you. Don't reinstall all the time, rather use a 
+virtulenv that has a link to the code.
+
+```
+python setup.py develop
+```
+
+When you submit a pull request it will be tested in travis. Once 
+the build is green the review can start. Please try to keep your 
+branch up to date to ensure you're not missing any tests. Larger 
+commits need to be discussed in github before we can accept them.  
+
+### Generating New Documentation
+
+Updating documentation is currently a manual step. From the `docs` folder:
+
+```
+pdoc --html --overwrite evol
+cp -rf evol/* .
+rm -rf evol
+```
+
+If you want to confirm that it works you can open the `index.html` file.
+
 You can generate the docs by running the following code in the
 /docs folder. 
 
