3a17688ccdaf289751ee75ff9cfc1401cba9373b
Rogier van der Geer <rogiervandergeer@godatadriven.com>
2017-12-09 09:36:03 +0100

    Repo has moved. PEP8

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup
 
-setup(name='evol',
-      version='0.1.1',
-      description='A Grammar for Evolutionary Algorithms and Heuristics',
-      author=['Vincent D. Warmerdam', 'Rogier van der Geer'],
-      author_email='vincentwarmerdam@gmail.com',
-      url='https://github.com/koaning/evol',
-      packages=['evol', 'evol.helpers'],
-      keywords=['genetic', 'algorithms', 'heuristics'])
+setup(
+    name='evol',
+    version='0.1.1',
+    description='A Grammar for Evolutionary Algorithms and Heuristics',
+    author=['Vincent D. Warmerdam', 'Rogier van der Geer'],
+    author_email='vincentwarmerdam@gmail.com',
+    url='https://github.com/godatadriven/evol',
+    packages=['evol', 'evol.helpers'],
+    keywords=['genetic', 'algorithms', 'heuristics']
+)
