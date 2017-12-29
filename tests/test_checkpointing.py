cc958d198a02459b159f9902ebcca81343764085
Rogier van der Geer <rogiervandergeer@godatadriven.com>
2017-12-29 09:48:34 +0100

    Refactor checkpointing tests

--- tests/test_checkpointing.py
+++ tests/test_checkpointing.py
@@ -4,20 +4,17 @@ from pytest import raises
 from evol import Population, Evolution
 
 
-class TestCheckpoint:
-
-    @property
-    def methods(self):
-        return 'pickle', 'json'
+class TestPickleCheckpoint:
+    method = 'pickle'
+    extension = '.pkl'
+    exception = AttributeError
 
     def test_checkpoint(self, tmpdir):
         directory = tmpdir.mkdir("ckpt")
-        for method in self.methods:
-            pop = Population([{'x': 1, 'y': 2} for _ in range(100)], lambda x: x['x'])
-            pop.checkpoint(directory=directory, method=method)
-        assert len(listdir(directory)) == 2
-        assert sorted(listdir(directory))[0].endswith('.pkl')
-        assert sorted(listdir(directory))[1].endswith('.json')
+        pop = Population([{'x': 1, 'y': 2} for _ in range(100)], lambda x: x['x'])
+        pop.checkpoint(directory=directory, method=self.method)
+        assert len(listdir(directory)) == 1
+        assert listdir(directory)[0].endswith(self.extension)
 
     def test_unserializable_chromosome(self, tmpdir):
         directory = tmpdir.mkdir("ckpt")
@@ -27,23 +24,27 @@ class TestCheckpoint:
                 self.x = x
 
         pop = Population([UnSerializableChromosome(i) for i in range(10)], lambda x: x.x)
-        with raises(AttributeError):
-            pop.checkpoint(directory=directory, method='pickle')
-        with raises(TypeError):
-            pop.checkpoint(directory=directory, method='json')
+        with raises(self.exception):
+            pop.checkpoint(directory=directory, method=self.method)
 
     def test_load(self, tmpdir):
         directory = tmpdir.mkdir("ckpt")
-        for method in self.methods:
-            pop = Population([{'x': 1, 'y': 2} for _ in range(100)], lambda x: x['x'])
-            pop.checkpoint(directory=directory, method=method)
-            lpop = Population.load(directory, lambda x: x['x'])
-            assert len(pop) == len(lpop)
-            assert all(x.__dict__ == y.__dict__ for x, y in zip(pop, lpop))
+        pop = Population([{'x': 1, 'y': 2} for _ in range(100)], lambda x: x['x'])
+        pop.checkpoint(directory=directory, method=self.method)
+        lpop = Population.load(directory, lambda x: x['x'])
+        assert len(pop) == len(lpop)
+        assert all(x.__dict__ == y.__dict__ for x, y in zip(pop, lpop))
 
     def test_evolution(self, tmpdir):
         directory = tmpdir.mkdir("ckpt")
-        evo = Evolution().mutate(lambda x: x+1).checkpoint(directory=directory)
+        evo = Evolution().mutate(lambda x: x+1).checkpoint(directory=directory, method=self.method)
         pop = Population(range(100), lambda x: x)
         pop.evolve(evolution=evo, n=100)
         assert len(listdir(directory)) == 100
+
+
+class TestJsonCheckpoint(TestPickleCheckpoint):
+
+    method = 'json'
+    extension = '.json'
+    exception = TypeError
