413bc3fde71973b630c1e96369432e2c2d91b5f2
vincent <vincentwarmerdam@gmail.com>
2018-11-10 16:58:21 +0100

    init docstring fixed

--- evol/problems/functions/__init__.py
+++ evol/problems/functions/__init__.py
@@ -1,4 +1,7 @@
 """
+The `evol.problems.functions` part of the library contains
+simple problem instances that do with known math functions.
+
 The functions in here are typically inspired from wikipedia:
 https://en.wikipedia.org/wiki/Test_functions_for_optimization
 """
