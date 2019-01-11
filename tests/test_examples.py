9264effc4dcafe50bfd3068a50543b964e5fe195
Rogier van der Geer <rogiervandergeer@godatadriven.com>
2019-01-11 10:56:18 +0100

    Add "None" to n_workers parameter

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -4,11 +4,14 @@ from examples.number_of_parents import run_evolutionary
 from examples.rock_paper_scissors import run_rock_paper_scissors
 
 
-@mark.parametrize('concurrent_workers', [1, 2])
+N_WORKERS = [1, 2, None]
+
+
+@mark.parametrize('concurrent_workers', N_WORKERS)
 def test_number_of_parents(concurrent_workers):
     run_evolutionary(verbose=False, workers=concurrent_workers)
 
 
-@mark.parametrize('concurrent_workers', [1, 2])
+@mark.parametrize('concurrent_workers', N_WORKERS)
 def test_rock_paper_scissors(concurrent_workers):
     run_rock_paper_scissors(silent=True, n_iterations=15, concurrent_workers=concurrent_workers)
