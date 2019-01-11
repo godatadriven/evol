6afdfa55ddb95ad6d8c372e0d5f1fd6374994160
Rogier van der Geer <rogiervandergeer@godatadriven.com>
2019-01-11 10:51:42 +0100

    Add rock_paper_scissors to test_examples

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,5 +1,14 @@
+from pytest import mark
+
 from examples.number_of_parents import run_evolutionary
+from examples.rock_paper_scissors import run_rock_paper_scissors
 
 
-def test_number_of_parents():
-    run_evolutionary(verbose=False)
+@mark.parametrize('concurrent_workers', [1, 2])
+def test_number_of_parents(concurrent_workers):
+    run_evolutionary(verbose=False, workers=concurrent_workers)
+
+
+@mark.parametrize('concurrent_workers', [1, 2])
+def test_rock_paper_scissors(concurrent_workers):
+    run_rock_paper_scissors(silent=True, n_iterations=15, concurrent_workers=concurrent_workers)
