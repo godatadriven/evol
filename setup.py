ffd8f44195ba970361a5632b427b847312c855e1
vincent <vincentwarmerdam@gmail.com>
2020-01-02 15:24:24 +0100

    yet another flake8 version

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     keywords=['genetic', 'algorithms', 'heuristics'],
     python_requires='>=3.6',
     tests_require=[
-        "pytest>=3.3.1", "attrs==19.1.0", "flake8>=3.6.0"
+        "pytest>=3.3.1", "attrs==19.1.0", "flake8>=3.7.9"
     ],
     extras_require={
         "dev": ["Sphinx>=2.0.0", "pytest==3.3.1", "attrs==19.1.0", "flake8==3.6.0"]
