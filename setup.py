c3246a1b01b724ed03d8ab7505d32732b6a2d339
vincent <vincentwarmerdam@gmail.com>
2020-01-02 15:13:15 +0100

    more flake business

--- setup.py
+++ setup.py
@@ -40,13 +40,13 @@ setup(
     keywords=['genetic', 'algorithms', 'heuristics'],
     python_requires='>=3.6',
     tests_require=[
-        "pytest==3.3.1", "attrs==19.1.0"
+        "pytest==3.3.1", "attrs==19.1.0", "flake8==3.6.0"
     ],
     extras_require={
         "dev": ["Sphinx>=2.0.0"]
     },
     setup_requires=[
-        "flake8==3.6.0", "pytest-runner"
+        "pytest-runner"
     ],
     install_requires=[
         "multiprocess>=0.70.6.1"
